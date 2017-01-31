67e5caee6912994017e9b305204dbcdd77dbc7d4
Alejandro Pacheco <alepacheco99@gmail.com>
2017-01-31 16:10:34 +0100

    animations

--- public/main.js
+++ public/main.js
@@ -7,12 +7,14 @@ socket.on('messages', function(data) {
 function render (data) {
 
   var html = data.map(function(elem, index) {
-    return(`<div class="animated slideInLeft">
+    return(`<div class="slideInLeft">
               <strong>${elem.author}</strong>:
               <em>${elem.text}</em>
             </div>`);
   }).join(" ");
   document.getElementById('messages').innerHTML = html;
+  // Remove animated class of previous messages
+  $('.animated div:last-child').addClass('animated')
   return false;
 }
 
